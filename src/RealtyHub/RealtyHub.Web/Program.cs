435fcacdcf654c73ffd7088cbba9318e7735fa86
israel-bsi <masterisraelsilva@gmail.com>
2025-01-08 10:58:53 -0300

    [Feat] - Inicio do frontend

--- src/RealtyHub/RealtyHub.Web/Program.cs
+++ src/RealtyHub/RealtyHub.Web/Program.cs
@@ -1,8 +1,13 @@
 using RealtyHub.ServiceDefaults;
 using RealtyHub.Web.Components;
 using System.Globalization;
+using System.Net;
 using RealtyHub.Core.Services;
+using RealtyHub.Web;
+using RealtyHub.Web.Security;
 using RealtyHub.Web.Services;
+using Microsoft.AspNetCore.Components.Authorization;
+using MudBlazor.Services;
 
 CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
 CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("pt-BR");
@@ -19,10 +24,29 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Services.AddOutputCache();
+builder.Services.AddScoped<CookieHandler>();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CookieAuthenticationStateProvider>();
+builder.Services.AddScoped(x => (ICookieAuthenticationStateProvider)x.GetRequiredService<AuthenticationStateProvider>());
+builder.Services.AddMudServices();
+builder.Services.AddLocalization();
 
-builder.Services.AddHttpClient("", client => client.BaseAddress = new("http://apiservice"));
+builder.Services.AddHttpClient(Configuration.HttpClientName, client =>
+{
+    client.BaseAddress = new("http://apiservice");
+})
+.ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+{
+    UseDefaultCredentials = true,
+    CookieContainer = new CookieContainer(),
+    UseCookies = true
+})
+.AddHttpMessageHandler<CookieHandler>();
+
 
 var app = builder.Build();
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
+CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("pt-BR");
 
 if (!app.Environment.IsDevelopment())
 {
---
+++ src/RealtyHub/RealtyHub.Web/Security/ICookieAuthenticationStateProvider.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+
+namespace RealtyHub.Web.Security;
+
+public interface ICookieAuthenticationStateProvider
+{
+    Task<bool> CheckAuthenticatedAsync();
+    Task<AuthenticationState> GetAuthenticationStateAsync();
+    void NotifyAuthenticationStateChanged();
+}
