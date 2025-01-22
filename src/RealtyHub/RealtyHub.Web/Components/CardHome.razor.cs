d61a939d401219169e7b20f97ddb9cc8665bb876
israel-bsi <masterisraelsilva@gmail.com>
2025-01-21 21:53:29 -0300

    Feat[Implementado isolamento de código c# do component CardHome] - Criado um arquivo cs e adicionado o código c# dele no arquivo

---
+++ src/RealtyHub/RealtyHub.Web/Components/CardHome.razor.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using RealtyHub.Core.Models;
+
+namespace RealtyHub.Web.Components;
+
+public partial class CardHomeComponent : ComponentBase
+{
+    #region Parameters
+
+    [Parameter]
+    public string Class { get; set; } = string.Empty;
+
+    [Parameter]
+    public Property Property { get; set; } = new();
+
+    #endregion
+
+    #region Services
+
+    [Inject] 
+    public ISnackbar Snackbar { get; set; } = null!;
+
+    #endregion
+
+    #region Methods
+
+    public string GetSrcPhoto()
+    {
+        var photo = Property
+                        .PropertyPhotos
+                        .FirstOrDefault(p => p.IsThumbnail)
+                    ?? Property.PropertyPhotos.FirstOrDefault();
+
+        var fullName = $"{photo?.Id}{photo?.Extension}";
+
+        return $"{Configuration.BackendUrl}/photos/{fullName}";
+    }
+
+    #endregion
+}
