9160dfb13b2bfd824ebf7faf9f330b952eafd4ae
israel-bsi <masterisraelsilva@gmail.com>
2025-05-12 23:23:49 -0300

    Docs[Core] - Adicionado documentação nas configurações

--- Codigos/RealtyHub/RealtyHub.ApiService/Configuration.cs
+++ Codigos/RealtyHub/RealtyHub.ApiService/Configuration.cs
@@ -1,20 +1,64 @@
 ﻿namespace RealtyHub.ApiService;
 
+/// <summary>
+/// Representa as configurações globais da aplicação.
+/// </summary>
+/// <remarks>
+/// Esta classe contém configurações importantes, como a política de CORS, 
+/// caminhos para diretórios e configurações de e-mail utilizados pelo serviço.
+/// </remarks>
 public class Configuration
 {
+    /// <summary>
+    /// Nome da política de CORS utilizada pela aplicação.
+    /// </summary>
     public const string CorsPolicyName = "realtyhub";
+
+    /// <summary>
+    /// Obtém ou define o caminho para os arquivos de contratos.
+    /// </summary>
     public static string ContractsPath { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Obtém ou define o caminho para os arquivos de fotos.
+    /// </summary>
     public static string PhotosPath { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Obtém ou define o caminho para os templates de contrato.
+    /// </summary>
     public static string ContractTemplatesPath { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Obtém ou define o caminho para os relatórios.
+    /// </summary>
     public static string ReportsPath { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Obtém ou define o caminho para os logos.
+    /// </summary>
     public static string LogosPath { get; set; } = string.Empty;
 
+    /// <summary>
+    /// Configurações de e-mail utilizadas pela aplicação.
+    /// </summary>
     public static EmailConfiguration EmailSettings { get; set; } = new();
+
+    /// <summary>
+    /// Representa as configurações de e-mail.
+    /// </summary>
     public class EmailConfiguration
     {
+        /// <summary>
+        /// Email de origem utilizado para envio das mensagens.
+        /// </summary>
+        /// <value>O email de origem utilizado para envio das mensagens.</value>
         public string EmailFrom { get; set; } = "realtyhub.br@gmail.com";
+
+        /// <summary>
+        /// Senha do email de origem.
+        /// </summary>
+        /// <value>A senha do email de origem.</value>
         public string EmailPassword { get; set; } = string.Empty;
     }
-
-    
 }
